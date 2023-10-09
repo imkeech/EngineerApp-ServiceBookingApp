32636ae313591866d0a707017697f6ffe032c5c9
Keerthi Vasan S A <sakeerthi23@gmail.com>
2023-10-09 21:33:09 +0530

    suma

--- app/src/main/java/com/example/service_engineer/FirebasePushMessageHandler.kt
+++ app/src/main/java/com/example/service_engineer/FirebasePushMessageHandler.kt
@@ -17,7 +17,7 @@ import com.google.firebase.messaging.RemoteMessage
 
 class FirebasePushMessageHandler : FirebaseMessagingService() {
     private val CHANNEL_ID = "engineer-notification"
-    // suma change
+    // suma change - 2
 
     override fun onNewToken(token: String) {
         Log.d("fcm", "New Token: $token")
